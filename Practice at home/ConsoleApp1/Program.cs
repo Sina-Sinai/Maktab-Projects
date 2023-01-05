b750e4a155aebb8d7a138837de4b6f8e1612529a
Sina <64578430+Sina-Sinai@users.noreply.github.com>
2023-01-05 11:35:48 +0330

    Add files via upload

---
+++ Practice at home/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class People
+    {
+        public int PersonalId { get; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class Student : People, IStudent
+    {
+        public int StudentId { get; }
+
+        public int IStudentId { get; }
+    }
+
+    public interface IStudent {
+        public int IStudentId { get; }
+
+    }
+}
